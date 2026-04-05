3b1b030b085315e61f945da9b89e0c7f08147a60
NguyenSon0403 <son2004ntt@gmail.com>
2026-04-05 23:55:58 +0700

    feat : add productVariants

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use("/api/v1/users", require("./routes/users"));
 app.use("/", require("./routes/index"));
 app.use("/api/v1/roles", require("./routes/roles"));
 app.use("/api/v1/products", require("./routes/products"));
+app.use("/api/v1/productVariants", require("./routes/productVariants"));
 app.use("/api/v1/categories", require("./routes/categories"));
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/carts", require("./routes/carts"));
@@ -30,7 +31,7 @@ app.use("/api/v1/messages", require("./routes/messages"));
 
 //connect
 
-mongoose.connect("mongodb://localhost:27017/db_ecommerce", {});
+mongoose.connect("mongodb://127.0.0.1:27017/db_ecommerce?replicaSet=rs0&directConnection=true", {});
 mongoose.connection.on("connected", function () {
   console.log("connected");
 });
