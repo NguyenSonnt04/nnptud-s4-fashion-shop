91fcbb2497f21d5531fa0283c13e2a35f610afe0
NguyenSon0403 <son2004ntt@gmail.com>
2026-04-05 22:26:24 +0700

    upload image

---
+++ routes/upload.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+let { uploadImage } = require('../utils/uploadHandler')
+let path = require('path')
+
+router.get('/:filename', function (req, res, next) {
+    let pathFile = path.join(__dirname, '../uploads', req.params.filename)
+    res.sendFile(pathFile)
+})
+
+router.post('/one_file', uploadImage.single('file'), function (req, res, next) {
+    if (!req.file) {
+        res.status(404).send({
+            message: "file khong duoc de trong"
+        })
+        return
+    }
+    res.send({
+        filename: req.file.filename,
+        path: req.file.path,
+        size: req.file.size
+    })
+})
+router.post('/multiple_file', uploadImage.array('files'), function (req, res, next) {
+    if (!req.files) {
+        res.status(404).send({
+            message: "file khong duoc de trong"
+        })
+        return
+    }
+    res.send(req.files.map(f => {
+        return {
+            filename: f.filename,
+            path: f.path,
+            size: f.size
+        }
+    }))
+})
+
+module.exports = router
